fabdcddfe81000a661a52d5243e780ebc73c9b8b
Robert Nishihara <robertnishihara@gmail.com>
2016-10-27 11:57:13 -0700

    Build arrow and numbuf in setup.py.

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
-import sys
+import subprocess
+from setuptools import setup, find_packages, Extension
 
-from setuptools import setup, find_packages
+from setuptools.command.build_ext import build_ext as _build_ext
 
 # Because of relative paths, this must be run from inside numbuf/.
 
-setup(
-  name = "numbuf",
-  version = "0.1.dev0",
-  use_2to3=True,
-  packages=find_packages(),
-  package_data = {
-    "numbuf": ["libnumbuf.so"]
-  },
-  zip_safe=False,
-)
+class build_ext(_build_ext):
+  def run(self):
+    subprocess.check_call(["./setup.sh"])
+    subprocess.check_call(["./build.sh"])
+    _build_ext.run(self)
+
+setup(name="numbuf",
+      version="0.0.1",
+      packages=find_packages(),
+      package_data={"numbuf": ["libnumbuf.so"]},
+      cmdclass={"build_ext": build_ext},
+      zip_safe=False)
