431423d24b0dc1285febfab27495ca23ab787d20
Philipp Moritz <pcmoritz@gmail.com>
2016-10-27 22:06:41 -0700

    fix numbuf build and installation

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 import subprocess
 from setuptools import setup, find_packages, Extension
 
-from setuptools.command.install import install as _install
-
 # Because of relative paths, this must be run from inside numbuf/.
 
-class install(_install):
-  def run(self):
-    subprocess.check_call(["./setup.sh"])
-    subprocess.check_call(["./build.sh"])
-    _install.run(self)
+subprocess.check_call(["./setup.sh"])
+subprocess.check_call(["./build.sh"])
 
 setup(name="numbuf",
       version="0.0.1",
       packages=find_packages(),
       package_data={"numbuf": ["libnumbuf.so"]},
-      cmdclass={"install": install},
+      include_package_data=True,
       zip_safe=False)
